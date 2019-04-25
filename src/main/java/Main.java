6dffe7ad4ac02eac5ae0cdde10cc2ca224ab15f8
mbalcer <mateusz75319@gmail.com>
2019-04-25 10:07:20 +0200

    Moving stage using body dragging

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,7 @@
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 
@@ -10,10 +12,32 @@ import javafx.stage.StageStyle;
 
 public class Main extends Application {
 
+    private static double xOffset = 0;
+    private static double yOffset = 0;
+
+    private void draggableWindow(AnchorPane anchorPane, final Stage stage) {
+        anchorPane.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                xOffset = stage.getX() - event.getScreenX();
+                yOffset = stage.getY() - event.getScreenY();
+            }
+        });
+
+        anchorPane.setOnMouseDragged(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                stage.setX(event.getScreenX() + xOffset);
+                stage.setY(event.getScreenY() + yOffset);
+            }
+        });
+    }
+
     @Override
     public void start(Stage primaryStage) throws Exception{
         FXMLLoader loader = new FXMLLoader(this.getClass().getResource("/view/mainView.fxml"));
         AnchorPane anchorPane = loader.load();
+        draggableWindow(anchorPane, primaryStage);
         Scene scene = new Scene(anchorPane);
         primaryStage.setTitle("WeatherApp");
         primaryStage.setScene(scene);
